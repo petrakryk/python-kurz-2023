fa6bc1866a9b526555f6dd0ff26c3f5b81535110
Petra Kryk <petra.krykorkova@gmail.com>
2023-02-07 22:43:26 +0100

    úkol 3 bonus

---
+++ úkol-03bonus.py
@@ -0,0 +1,35 @@
+import json
+
+with open('/Users/petrakrykorkova/Documents/body.json') as vstup:
+    data = vstup.read()
+
+prospech = json.loads(data)
+
+with open('/Users/petrakrykorkova/Documents/bonusy2.json') as vstup_bonusy:
+    bonusy_data = vstup_bonusy.read()
+
+bonusy = json.loads(bonusy_data)
+
+hodnoceni = {
+    "1": (90, 100),
+    "2": (70, 89),
+    "3": (50, 69),
+    "4": (30, 49),
+    "5": (0, 29)
+}
+
+znamky = {}
+
+for item in prospech:
+    if item in bonusy:
+        prospech[item] = prospech[item] + bonusy[item]
+    
+    for znamka in hodnoceni:
+        low = hodnoceni[znamka][0]
+        top = hodnoceni[znamka][1]
+
+        if (prospech[item] >= low) and (prospech[item] < top):
+            znamky.update({item: znamka})
+
+with open("znamky.json", mode="w", encoding="utf-8") as vystup:
+    json.dump(znamky, vystup, ensure_ascii=False)
